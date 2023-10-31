36c331fcde4bcad49400ad7bf9f243904a9d2dd2
John Doe <you@example.com>
2023-10-30 21:15:37 -0400

    commit changes

--- bank-activity-lab/code/StockMetrics.py
+++ bank-activity-lab/code/StockMetrics.py
@@ -18,16 +18,55 @@ class StockMetrics(StockData):
         """
         averages = []
         for row in self.data:
-            ...
-
+            valid_price = []
+            for num in row[1:]:
+                num = num.strip()
+    
+                if num and num.replace('.','', 1).isdigit():
+                    price = float(num)
+                    valid_price.append(price)
+            sum_average = sum(valid_price) / len(valid_price)
+            averages.append(round(sum_average,3))
+            print(averages)
+        
         return averages
 
     def median02(self):
         """pt2
         """
-        ...
+        median = []
+        for row in self.data:
+            valid_price = []
+            for num in row[1:]:
+                num = num.strip()
+    
+                if num and num.replace('.','', 1).isdigit():
+                    price = float(num)
+                    valid_price.append(price)
+                    
+            sum_median = stats.median(valid_price)
+            median.append(sum_median)
+            print(median)
+        
+        return median
+
 
     def stddev03(self):
         """pt3
         """
-        ...
+        stddev = []
+        for row in self.data:
+            valid_price = []
+            for num in row[1:]:
+                num = num.strip()
+    
+                if num and num.replace('.','', 1).isdigit():
+                    price = float(num)
+                    valid_price.append(price)
+                    
+            new_stddev = stats.stdev(valid_price)
+            stddev.append(round(new_stddev,3))
+            print(stddev)
+        
+        return stddev
+
